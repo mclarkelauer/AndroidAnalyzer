200304cd644e2f7940590fd18d4f80b189dbd72e
Matt Clarke-Lauer <matt@clarkelauer.com>
2015-06-18 09:51:16 -0400

    remove shell scripts

--- processAllDex.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for d in Tests/Test_Cases/AllBinaries/*
-do
-	python DexScope.py "$d/classes.dex"
-done
--- processApks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for d in ~/Desktop/apks/*.apk
-do
-	python DexScope.py -a $d
-done
