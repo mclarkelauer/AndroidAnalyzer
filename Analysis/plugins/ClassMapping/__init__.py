814bc1b2cb474a5b8ff21a048ef2bab41949eac8
Matt Clarke-Lauer <matt@clarkelauer.com>
2015-06-18 09:44:15 -0400

    adding empty class mapping plugin

---
+++ Analysis/plugins/ClassMapping/__init__.py
@@ -0,0 +1,40 @@
+__author__ = 'Matt Clarke-Lauer'
+__email__ = 'matt@clarkelauer.com'
+__credits__ = ['Matt Clarke-Lauer']
+__date__ = 8 / 1 / 13
+
+__version__ = '0.1'
+__status__ = 'Development'
+
+
+import pdb
+
+'''
+Class Mapping
+
+Generate class signature that can be used to identify it
+'''
+
+name = "ClassMapGenerator"
+description = "Generate Class signature"
+result = []
+enabled = False
+writeClassMapToFile = False
+
+def getName():
+  "return analysis name"
+  return name
+
+def getDescription():
+  "return analysis description"
+  return description
+
+def getResults(results):
+  "add result of analysis to result dictionary"
+  results["Class Maps"] = result
+  return results
+
+def run(dependencies,classes):
+  import pprint
+  pp = pprint.PrettyPrinter(indent=2)
+  pp.pprint(classes[classes.keys()[2]])
